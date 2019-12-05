289e6bb4fdf2019146e37272d5f377a1969d4524
powhapki <powhapki@gmail.com>
2019-12-05 23:49:55 +0900

    Create variables.tf

---
+++ org_test/variables.tf
@@ -0,0 +1,42 @@
+# Every variables in here can be set as a Terraform Variable at TFE. 
+# Using Terraform Variables on TFE is more secure way and recommended.
+variable "hostname" {
+    default = "app.terraform.io"
+}
+
+variable "token" {
+    default = "TFE TOKEN"
+}
+
+variable "ws_name" {
+    default = "c-w-005"
+}
+
+variable "vd_org" {
+    default = "sec-vd-test001"
+}
+
+variable "user_name" {
+    default = "c-w-005"
+}
+
+variable "vcs_token" {
+    default = "OAuth Token ID from TFE VCE Integration"
+}
+
+variable "repo_id" {
+    default = "jsp-hashicorp/jsp-hashicorp.github.io"
+}
+
+# This variables can be set as an Environment Variables at TFE either sensitive or not.
+variable "region" {
+    default = "ap-northeast-2"
+}
+
+variable "access_key" {
+    default = "AWS_ACCESS_KEY"
+}
+
+variable "secret_key" {
+    default = "AWS_SECRET_ACCESS_KEY"
+}
