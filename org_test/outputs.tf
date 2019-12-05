be7c30d88cc37efdd903dd81aa5da571c463bad3
powhapki <powhapki@gmail.com>
2019-12-05 23:47:08 +0900

    Create outputs.tf

---
+++ org_test/outputs.tf
@@ -0,0 +1,20 @@
+# Result of creating Workspace and setting ENV. Variable
+#output "Org_name" {
+#    value = tfe_organization.test.name
+#}
+
+#output "Org_token" {
+#    value = tfe_organization_token.test.token
+#}
+
+output "workspace_id" {
+    value = tfe_workspace.test.id
+}
+
+#output "access-key" {
+#    value = tfe_variable.access_key.value
+#}
+
+#output "secret-key" {
+#    value = tfe_variable.secret_key.value
+#}
